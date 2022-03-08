70c6b71717e843fcd6a79eadcccb5b60dfcd57f4
m-triple-m <triplem656@gmail.com>
2022-03-08 11:48:32 +0530

    ..

--- OnlineNewsPortal/backend/config.js
+++ OnlineNewsPortal/backend/config.js
@@ -1,6 +1,7 @@
-const api_config = {
-    db_url:
-      "mongodb+srv://mmm:mmm@cluster0.gvyon.mongodb.net/mernwss1100?retryWrites=true&w=majority",
-  };
-  
-  module.exports = api_config;
+const api_config={
+    db_url:'mongodb+srv://Shivangi:shivangi@cluster0.8c15g.mongodb.net/mernwss1100?retryWrites=true&w=majority'
+}
+
+module.exports=api_config;
+
+
--- OnlineNewsPortal/frontend/src/App.js
+++ OnlineNewsPortal/frontend/src/App.js
@@ -1,9 +1,9 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import SideBar from "./components/admin/sidebar";
+import Login from "./components/main/login";
 import Reporter from "./components/reporter";
 import AddNews from "./components/reporter/addNews";
-import Login from "./components/main/login";
 import ReporterDashbord from "./components/reporter/dashboard";
 
 function App() {
@@ -14,7 +14,6 @@ function App() {
           <Route element={<Reporter />} path="reporter">
             <Route element={<ReporterDashbord />} path="dashboard">
               <Route element={<AddNews />} path="addnews" />
-              
             </Route>
           </Route>
           <Route element={<Login />} path="/login" />
