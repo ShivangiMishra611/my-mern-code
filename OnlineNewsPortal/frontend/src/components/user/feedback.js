51609348451c611893d88aaa6b8935984ede186e
m-triple-m <triplem656@gmail.com>
2022-04-03 11:53:22 +0530

    ..

--- OnlineNewsPortal/frontend/src/components/user/feedback.js
+++ OnlineNewsPortal/frontend/src/components/user/feedback.js
@@ -1,28 +1,20 @@
 import { Formik } from "formik";
 import { useState } from "react";
 import app_config from "../../config";
-import {
-  
-  TextField
-} from "@mui/material";
+import { TextField } from "@mui/material";
 import Swal from "sweetalert2";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import InputAdornment from '@mui/material/InputAdornment';
-
-
-
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import InputAdornment from "@mui/material/InputAdornment";
 
 const Feedback = () => {
   const url = app_config.api_url;
 
- 
   // const img1="image1.jpg"
   const feedbackForm = {
-    user:"",
+    user: "",
   };
 
   const feedbackSubmit = (values) => {
-    
     console.log(values);
 
     fetch(url + "/feedback/add", {
@@ -33,59 +25,44 @@ const Feedback = () => {
       },
     }).then((res) => {
       console.log(res.status);
-        if (res.status === 200) {
-         
-          Swal.fire({
-            icon: "success",
-            title: "success",
-            text: "Feedback added Successfully",
-          });
-        }
-     
-        else if (res.status === 300) {
-          Swal.fire({
-            icon: "error",
-            title: "failed",
-            text: " failed",
-          });
-        }
-        
-        return res.json();
+      if (res.status === 200) {
+        Swal.fire({
+          icon: "success",
+          title: "success",
+          text: "Feedback added Successfully",
         });
-    };
-  
-    
- 
+      } else if (res.status === 300) {
+        Swal.fire({
+          icon: "error",
+          title: "failed",
+          text: " failed",
+        });
+      }
+
+      return res.json();
+    });
+  };
 
   const validate = (values) => {
     const errors = {};
 
-   
     if (!values.user) {
       errors.user = "Required";
-    }else if(  !/^[A-Za-z]+/i.test(values.user)
-
-    )
-    {
-      errors.user="Invalid User";
+    } else if (!/^[A-Za-z]+/i.test(values.user)) {
+      errors.user = "Invalid User";
     }
-   
-    return errors;
 
+    return errors;
   };
 
-  
-
   return (
-    
-      <Formik initialValues={feedbackForm} onSubmit={feedbackSubmit}>
-        {({ values, handleChange, handleSubmit,errors }) => (
-          <form onSubmit={handleSubmit}>
-            
-              <h5 className="card-header">Feedback </h5>
-              <div className="card-body">
-                <div className="mb-3">
-                <TextField
+    <Formik initialValues={feedbackForm} onSubmit={feedbackSubmit}>
+      {({ values, handleChange, handleSubmit, errors }) => (
+        <form onSubmit={handleSubmit}>
+          <h5 className="card-header">Feedback </h5>
+          <div className="card-body">
+            <div className="mb-3">
+              <TextField
                 className="w-100 mt-3"
                 placeholder="User"
                 label="user"
@@ -95,7 +72,6 @@ const Feedback = () => {
                 onChange={handleChange}
                 value={values.user}
                 error={errors.user}
-                              
                 InputProps={{
                   endAdornment: (
                     <InputAdornment position="end">
@@ -106,24 +82,21 @@ const Feedback = () => {
                   ),
                 }}
                 helperText={errors.user}
-                
               />
-                </div>
-                <button 
-                type="submit" 
-                className="btn btn-primary"
-                color ="success"
-                variant="contained">
-                  Submit
-                </button>
-
-</div>
-                
-          </form>
-        )}
-      </Formik>
-  )  
-                
-};          
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              color="success"
+              variant="contained"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      )}
+    </Formik>
+  );
+};
 
 export default Feedback;
