9f4974a0846a64b5de3f63fc9e636c23e15d64a6
m-triple-m <triplem656@gmail.com>
2022-04-24 13:13:29 +0530

    ..

--- OnlineNewsPortal/frontend/src/components/admin/manageNews.js
+++ OnlineNewsPortal/frontend/src/components/admin/manageNews.js
@@ -1,5 +1,18 @@
 import { useEffect, useState } from "react";
-import { Button } from "@mui/material";
+import {
+  Autocomplete,
+  Button,
+  Card,
+  CardContent,
+  Chip,
+  FormControl,
+  InputAdornment,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Tooltip,
+} from "@mui/material";
 import toast, { Toaster } from "react-hot-toast";
 import app_config from "../../config";
 import Accordion from "@mui/material/Accordion";
@@ -8,13 +21,19 @@ import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Stack from "@mui/material/Stack";
-import Fab from '@mui/material/Fab';
-import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded';
-import BeenhereRoundedIcon from '@mui/icons-material/BeenhereRounded';
+import Fab from "@mui/material/Fab";
+import DeleteRoundedIcon from "@mui/icons-material/DeleteRounded";
+import BeenhereRoundedIcon from "@mui/icons-material/BeenhereRounded";
+import { Formik } from "formik";
+import Swal from "sweetalert2";
+import { Edit, TitleSharp, Category, Newspaper } from "@mui/icons-material";
 
 const ManageNews = () => {
   const [NewsArray, setNewsArray] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const [showUpdateForm, setShowUpdateForm] = useState(false);
+  const [updateFormdata, setUpdateFormdata] = useState({});
 
   const url = app_config.api_url;
 
@@ -74,43 +93,254 @@ const ManageNews = () => {
             <h4>{news.title}</h4>
           </AccordionSummary>
           <AccordionDetails>
-          <img src={url + "/" + news.thumbnail} height="200" />
-          <br></br>
+            <img src={url + "/" + news.thumbnail} height="200" />
             <br></br>
-          <h5>{news.summary}</h5>
+            <br></br>
+            <h5>{news.summary}</h5>
 
             <Typography>{news.category}</Typography>
+            <Stack direction="row" spacing={2}>
+              <Fab
+                disabled={news.approvenews}
+                variant="extended"
+                size="small"
+                color="primary"
+                onClick={(e) => approveNews(news._id)}
+                aria-label="add"
+              >
+                <BeenhereRoundedIcon sx={{ mr: 1 }} />
+                {news.approvenews ? "Approved" : "Approve News"}
+              </Fab>
+
+              <Fab
+                variant="extended"
+                size="small"
+                color="primary"
+                onClick={(e) => deleteNews(news._id)}
+                aria-label="add"
+              >
+                <DeleteRoundedIcon sx={{ mr: 1 }} />
+                Delete News
+              </Fab>
+              <Tooltip title="Update News Article">
+                <Fab
+                  size="medium"
+                  color="success"
+                  onClick={(e) => {
+                    setUpdateFormdata(news);
+                    setShowUpdateForm(true);
+                  }}
+                  aria-label="add"
+                >
+                  <Edit sx={{ margin: 1 }} />
+                </Fab>
+              </Tooltip>
+            </Stack>
           </AccordionDetails>
-          <Stack direction="row" spacing={2}>
-          <Fab
-             disabled={news.approvenews}
-              variant="extended"
-              size="small"
-              color="primary"
-              onClick={(e) => approveNews(news._id)}
-              aria-label="add"
-            >
-               < BeenhereRoundedIcon sx={{ mr: 1 }} />
-              {news.approvenews ? "Approved" : "Approve News"}
-              
-           
-            </Fab>
-
-
-            <Fab
-              variant="extended"
-              size="small"
-              color="primary"
-              onClick={(e) => deleteNews(news._id)}
-              aria-label="add"
-            >
-              < DeleteRoundedIcon sx={{ mr: 1 }} />
-              Delete News
-            </Fab>
-
-          </Stack>
         </Accordion>
       ));
+    }
+  };
+
+  const submitNews = (values) => {
+    // values.thumbnail = thumbnail;
+    console.log(values);
+
+    fetch(url + "/news/update/" + values._id, {
+      method: "PUT",
+      body: JSON.stringify(values),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      console.log(res.status);
+      if (res.status === 200) {
+        Swal.fire({
+          icon: "success",
+          title: "success",
+          text: "News Updated Successfully",
+        });
+        fetchData();
+        setShowUpdateForm(false);
+      }
+      return res.json();
+    });
+  };
+
+  const updateForm = () => {
+    if (showUpdateForm) {
+      return (
+        <div>
+          <Card>
+            <CardContent sx={{ width: 640 }}>
+              <Formik
+                initialValues={updateFormdata}
+                onSubmit={submitNews}
+                // validationSchema={validationSchema}
+              >
+                {({ values, handleChange, handleSubmit, errors }) => (
+                  <form onSubmit={handleSubmit}>
+                    <h5 className="card-header">Add News</h5>
+
+                    <div className="card-body">
+                      <TextField
+                        className="w-100 mt-3"
+                        placeholder="Title"
+                        label="Title"
+                        variant="outlined"
+                        id="title"
+                        onChange={handleChange}
+                        value={values.title}
+                        error={Boolean(errors.title)}
+                        helperText={errors.title}
+                        InputProps={{
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <TitleSharp
+                                sx={{
+                                  color: "active.active",
+                                  mr: 1,
+                                  my: 0.5,
+                                }}
+                              />
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+
+                      <br></br>
+                      <br></br>
+
+                      <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label1">
+                          Category
+                        </InputLabel>
+
+                        <Select
+                          labelId="demo-simple-select-label1"
+                          id="category"
+                          name="category"
+                          label="Category"
+                          value={values.category}
+                          error={Boolean(errors.category)}
+                          helperText="Category is required"
+                          onChange={handleChange}
+                          InputProps={{
+                            endAdornment: (
+                              <InputAdornment position="end">
+                                <Category
+                                  sx={{
+                                    color: "active.active",
+                                    mr: 1,
+                                    my: 0.5,
+                                  }}
+                                />
+                              </InputAdornment>
+                            ),
+                          }}
+                        >
+                          {["A", "B", "C"].map((category) => (
+                            <MenuItem value={category}>{category}</MenuItem>
+                          ))}
+                        </Select>
+                      </FormControl>
+                      <br></br>
+                      <br></br>
+
+                      <TextField
+                        className="w-100 mt-3"
+                        label="Add News"
+                        multiline
+                        rows={4}
+                        variant="outlined"
+                        id="summary"
+                        onChange={handleChange}
+                        value={values.summary}
+                        error={Boolean(errors.summary)}
+                        helperText={errors.summary}
+                        aria-label="Add News"
+                        placeholder="Add News"
+                        InputProps={{
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <Newspaper
+                                sx={{
+                                  color: "active.active",
+                                  mr: 1,
+                                  my: 0.5,
+                                }}
+                              />
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+
+                      <br></br>
+                      <br></br>
+
+                      <Autocomplete
+                        className="mt-5"
+                        multiple
+                        id="tags"
+                        options={["crime", "politics", "sports"].map(
+                          (topic) => topic
+                        )}
+                        freeSolo
+                        renderTags={(value, getTagProps) =>
+                          value.map((option, index) => (
+                            <Chip
+                              variant="filled"
+                              label={option}
+                              {...getTagProps({ index })}
+                            />
+                          ))
+                        }
+                        renderInput={(params) => (
+                          <TextField
+                            id="tags"
+                            value={values.tags}
+                            error={Boolean(errors.tags)}
+                            helperText={errors.tags}
+                            onChange={handleChange}
+                            {...params}
+                            variant="filled"
+                            label="Add Relevant Tags"
+                          />
+                        )}
+                      />
+
+                      <br></br>
+                      <br></br>
+
+                      <div className="mb-3">
+                        <label for="formFile" class="form-label">
+                          Add Image
+                        </label>
+                        <input
+                          className="form-control"
+                          type="file"
+                          // onChange={uploadThumbnail}
+                        />
+                      </div>
+
+                      <button type="submit" className="btn btn-primary">
+                        Submit
+                      </button>
+                      <button
+                        onClick={(e) => setShowUpdateForm(false)}
+                        type="button"
+                        className="btn btn-primary"
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  </form>
+                )}
+              </Formik>
+            </CardContent>
+          </Card>
+        </div>
+      );
     }
   };
 
@@ -119,6 +349,8 @@ const ManageNews = () => {
       <Toaster position="top-right" reverseOrder={false} />
 
       {displayNews()}
+
+      {updateForm()}
     </div>
   );
 };
