789f8e6ed3be9120b3d423e05d4262112e53f314
m-triple-m <triplem656@gmail.com>
2022-04-28 19:39:03 +0530

    ..

--- OnlineNewsPortal/frontend/src/components/main/viewNews.js
+++ OnlineNewsPortal/frontend/src/components/main/viewNews.js
@@ -1,44 +1,26 @@
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import app_config from "../../config";
 
 const ViewNews = () => {
-    const id = useParams();
-    const url = app_config.api_url;
+  const id = useParams();
+  const url = app_config.api_url;
 
+  const fetchData = () => {
+    fetch(url + "/news/getbyid/" + id)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
 
-    const fetchData = () => {
-        fetch(url + "/news/getbyid/"+id)
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data);
-            
-           
-          });
-      };
-    
-      const refreshData = () => {
-       
-        fetch(url + "/news/approvenews")
-          .then((res) => res.json())
-          .then((data) => {
-            console.log(data);
-            
-          });
-      };
-      
-    
+  useEffect(() => {
+    fetchData();
+  }, []);
 
+  const displayNews = () => {};
 
-
-
-
-    
-    return(
-        <div>
-
-        </div>
-    );
-
+  return <div></div>;
 };
 
-export default  ViewNews
+export default ViewNews;
