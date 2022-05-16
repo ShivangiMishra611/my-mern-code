a5cbaf1deb676f3dffede843a415ff223b7f9d67
m-triple-m <triplem656@gmail.com>
2022-05-16 11:57:00 +0530

    ..

---
+++ OnlineNewsPortal/frontend/src/components/adminAuthenticator.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+
+const AdminAuthenticator = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(sessionStorage.getItem("admin"))
+  );
+
+  const navigate = useNavigate();
+
+  console.log(currentUser);
+
+  if (!currentUser) {
+    Swal.fire({
+      icon: "info",
+      title: "OOops!!",
+      text: "You need to be logged in",
+    });
+
+    return <Navigate to="/main/login" />;
+  } else if (currentUser) {
+    console.log(currentUser);
+    if (currentUser.isAdmin) {
+      navigate("/admin");
+    } else {
+      navigate("/reporter");
+    }
+  }
+
+  return children;
+};
+
+export default AdminAuthenticator;
