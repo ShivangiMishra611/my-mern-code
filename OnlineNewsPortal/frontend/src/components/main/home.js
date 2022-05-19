488ee1d02e17f4cac6dd2323400dbba1d4653c43
nikita1-crra <69074962+nikita1-crra@users.noreply.github.com>
2022-05-19 05:58:49 -0800

    ...

--- OnlineNewsPortal/frontend/src/components/main/home.js
+++ OnlineNewsPortal/frontend/src/components/main/home.js
@@ -8,6 +8,10 @@ import { useEffect, useState } from "react";
 import app_config from "../../config";
 import React from "react";
 import TimeAgo from "javascript-time-ago";
+import {
+  Grid,
+  CardMedia,
+} from "@mui/material";
 
 const Home = () => {
   const url = app_config.api_url;
@@ -1134,10 +1138,18 @@ const Home = () => {
 
 
 
-                      <div className="header-cat "  style={{mb:2}} >
+                      <div className= "header-cat "  >
                       <br></br>
-                      
-                        <h1><b>CATEGORIES</b></h1>
+                     
+                        <h3><b>CATEGORIES</b></h3>
+                        <Grid item xs={6} md={7}>
+                        <CardMedia
+                          component="img"
+                          height="100"
+                          image={url + "/images/lock.jpg"}
+                        />
+                      </Grid>
+                        
                         <ul className="vertical-menu">
                           <li>
                             <NavLink
